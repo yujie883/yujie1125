7361fc068e9de037d2ce7834ecd27fc4cea20a2c
yujie883 <yujiewu883@gmail.com>
2024-12-27 00:08:02 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 let i=0,imgArr=new Array();
-imgArr[0] = "https://kpopping.com/documents/84/4/800/240413-SEVENTEEN-DK-Golden-Wave-in-Taiwan-documents-6.jpeg?v=73859"
-imgArr[1] ="https://phinf.wevpstatic.net/MjAyNDA2MjhfMzAg/MDAxNzE5NTY1MDIzODM2.x6z4HGrQSkHDjeNAk__w7KXaS1kJvvsKizuWkl8WNnQg.O-SWbC8k9MCVYvP2qfJZAiqg3NRU_02A1ei3lG8J0jog.JPEG/f2881f4f-b406-49e6-974c-25b4b8844633.jpeg"
-imgArr[2] ="https://i.pinimg.com/736x/6e/5c/a6/6e5ca6a5f9335ecfdd2a050276071d7a.jpg"
-  
+imgArr[0] = "https://kpopping.com/documents/14/4/241123-Seventeen-DK-at-2024-MAMA-Awards-Day-3-documents-6.jpeg?v=fcdd9"
+imgArr[1] ="https://www.pinkvilla.com/images/2024-06/861192119_svt-2.jpg"
+imgArr[2] ="https://i.pinimg.com/736x/82/7b/43/827b43675e6d5626c16fac2aff0a5a85.jpg"
+imgArr[3] ="https://i.pinimg.com/736x/b4/9e/7e/b49e7eed971d4a7c351537501251bc1c.jpg"  
 function showImg(){
  document.getElementById('ico').src = imgArr[i];
-  i = (i+1) % 3 ;
+  i = (i+1) % 4 ;
 }
 function show(){
   setInterval(showImg,2000);
+}
+let box = document.querySelector('.ncyu');
+box.style = "background:black; color:white; top:10px;left:10px";
+document.getElementById("btn").addEventListener("click",lee);
+function lee(){
+  document.getElementById("aa").innerHTML = "要放寒假了!";
+  document.getElementById("aa").style.color = "red";
+  document.getElementById("btn").innerHTML = "請按一下";
+  document.getElementById("btn").style.color= "blue";
 }
